c5d95a415ee4b5629105f3f1c8c2ad1176dbf3b5
Valeriy Bogdanov <valerabogdanov@inbox.ru>
2023-11-28 10:49:23 +0600

    fix mobile button

--- js/main.js
+++ js/main.js
@@ -1,8 +1,9 @@
 const menu = document.querySelector(".mobile-menu");
 const mMenuToggle = document.querySelector(".mobile-menu-wrapper");
-const mMenuBurger = document.querySelector('.mobile-menu-burger')
+const mMenuBurger = document.querySelector(".mobile-menu-burger");
 
 mMenuToggle.addEventListener("click", (event) => {
+  event.preventDefault()
   menu.classList.toggle("active");
   mMenuBurger.classList.toggle("active");
 });
